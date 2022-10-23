730bbcdb94b135efbbb4ec6b4800d9488a9673cd
JoshWags <101152131+JoshWags@users.noreply.github.com>
2022-10-23 16:16:58 +1100

    Initial commit before updates
    Files will be updated in next commit.

---
+++ GroupTherapyWebApp/CONTROLLERS/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using $safeprojectname$.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace $safeprojectname$.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Family_Group()
+        {
+            return View();
+        }
+
+        public IActionResult Member_Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ GroupTherapyWebApp/MODELS/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GroupTherapyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
