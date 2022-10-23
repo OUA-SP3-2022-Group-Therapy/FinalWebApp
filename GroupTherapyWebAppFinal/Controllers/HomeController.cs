4db072d5c724faec3edb9e8b2101a1c8220ad946
JoshWags <101152131+JoshWags@users.noreply.github.com>
2022-10-23 19:11:24 +1100

    Added code back in

---
+++ GroupTherapyWebAppFinal/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GroupTherapyWebAppFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Family_Group()
+        {
+            return View();
+        }
+
+        public IActionResult Member_Profile()
+        {
+            return View();
+        }
+
+        public IActionResult Pet_Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ GroupTherapyWebAppFinal/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupTherapyWebAppFinal.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? UserType { get; set; }
+        public string? Gender { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateCreated { get; set; }
+
+        public UserModel()
+        {
+
+        }
+    }
+}
