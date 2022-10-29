ad24fa39d0d330f8548702353dc972deb88b4a3c
JoshWags <101152131+JoshWags@users.noreply.github.com>
2022-10-29 16:57:26 +1100

    Database attempt 2
    Still being created but some progress was made.

---
+++ GroupTherapyWebAppFinal/Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GroupTherapyWebAppFinal.Models;
+
+namespace GroupTherapyWebAppFinal.Data
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => CreateHostBuilder(args).Build().Run();
+
+        // EF Core uses this method at design time to access the DbContext
+        public static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(
+                    webBuilder => webBuilder.UseStartup<Startup>());
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+            => services.AddDbContext<ApplicationDbContext>();
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //public DbSet<UserModel> UserModel { get; set; }
+    }
+}
